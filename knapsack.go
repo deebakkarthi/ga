a604b530c9e4b683fce9178dd25c91b017356713
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-09-12 13:28:54 +0530

    Add: Knapsack

--- knapsack.go
+++ knapsack.go
@@ -6,45 +6,53 @@ import (
 	"sort"
 )
 
-type Gene = byte
+type Gene = float32
 
 type DNA struct {
-	genes [geneLen]Gene
+	genes []Gene
 }
 
-const charset = "abcdefghijklmnopqrstuvwxyz0123456789 @"
-const goalString string = "elambharathi is a business man"
-const geneLen = len(goalString)
 const mutRate = 0.01
 const popSize = 150
 
+var weights []float32 = []float32{10, 20, 30}
+var benefits []float32 = []float32{60, 100, 120}
+
+const maxWeight float32 = 50
+
+var numItems int = len(weights)
+
 func geneGen() Gene {
-	return Gene(charset[rand.Intn(len(charset))])
+	return Gene(rand.Float32())
 }
 
 func populationGen(n int) []DNA {
 	ret := make([]DNA, n)
 	for i := 0; i < n; i++ {
 		var tmp DNA
-		for j := 0; j < geneLen; j++ {
-			tmp.genes[j] = geneGen()
+		for range weights {
+			tmp.genes = append(tmp.genes, geneGen())
 		}
 		ret[i] = tmp
 	}
 	return ret
 }
 
-func fitness(d DNA) int {
-	tmp := 0
+func fitness(d DNA) float32 {
+	var tmp float32
+	var totalWeight float32
 	for i, v := range d.genes {
-		if v == goalString[i] {
-			tmp++
-		}
+		tmp += v * benefits[i]
+		totalWeight += v * weights[i]
+	}
+	// Return -1 if the solution is overweight
+	if totalWeight > maxWeight {
+		return -1
 	}
 	return tmp
 }
 
-func selection(population *[]DNA, fitness func(DNA) int) []DNA {
+func selection(population *[]DNA, fitness func(DNA) float32) []DNA {
 	var matingPool []DNA
 	sort.Slice(*population, func(i, j int) bool {
 		return fitness((*population)[i]) < fitness((*population)[j])
@@ -59,8 +67,9 @@ func selection(population *[]DNA, fitness func(DNA) int) []DNA {
 
 func crossover(a, b DNA) DNA {
 	var child DNA
-	midpoint := rand.Intn(geneLen)
-	for i := 0; i < geneLen; i++ {
+	child.genes = make([]float32, numItems)
+	midpoint := rand.Intn(len(a.genes))
+	for i := 0; i < len(a.genes); i++ {
 		if i < midpoint {
 			child.genes[i] = a.genes[i]
 		} else {
@@ -78,17 +87,23 @@ func mutate(d *DNA, mutRate float32) {
 	}
 }
 
+func (d DNA) equals(b DNA) bool {
+	for i, v := range d.genes {
+		if v != b.genes[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func reproduction(population *[]DNA, crossover func(a, b DNA) DNA,
 	mutate func(d *DNA, mutRate float32)) {
 	matingPool := selection(population, fitness)
 	for i := range *population {
 		var a, b DNA
-		for {
+		for a.equals(b) {
 			a = matingPool[rand.Intn(len(matingPool))]
 			b = matingPool[rand.Intn(len(matingPool))]
-			if a != b {
-				break
-			}
 		}
 		child := crossover(a, b)
 		mutate(&child, mutRate)
@@ -100,9 +115,9 @@ func reproduction(population *[]DNA, crossover func(a, b DNA) DNA,
 func main() {
 	population := populationGen(popSize)
 	generations := 0
-	for string(population[len(population)-1].genes[:]) != goalString {
+	for i := 0; i < 300; i++ {
 		reproduction(&population, crossover, mutate)
-		fmt.Printf("%s %d\n", population[len(population)-1], fitness(population[len(population)-1]))
+		fmt.Printf("%f %f\n", population[len(population)-1], fitness(population[len(population)-1]))
 		generations++
 	}
 	fmt.Println(generations)
